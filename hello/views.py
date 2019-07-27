31a885b94a751e15a290ff32ee6cccb9261af5f4
dcw3 <dcw3@princeton.edu>
2019-07-27 18:41:15 -0400

    test ab versioning

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 from .models import Greeting
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+	int ab_test_val = random.randint(0, 1);
+	if ab_test_val == 0:
+    	return render(request, "index.html")
+    else:
+    	return HttpResponse("hi I am B version")
 
 
 def db(request):
